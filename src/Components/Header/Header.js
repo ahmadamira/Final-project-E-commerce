3f580cf256b63a12c309cf7cf840006740959c38
ahmad amira <ahmad_amira@outlook.com>
2023-05-13 21:30:35 +0300

    created header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,172 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import CasesOutlinedIcon from '@mui/icons-material/CasesOutlined';
+import AdbIcon from '@mui/icons-material/Adb';
+
+
+const pages = ['Home', 'About', 'Contact us'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function ResponsiveAppBar() {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <AppBar position="static" sx={{ background: 'white' }}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 9,
+                            ml: 3,
+                            display: { xs: 'none', md: 'flex' },
+                            fontWeight: 700,
+                            color: 'black',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        NorthStar
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="black"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+
+                            color: 'black',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        NorthStar
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: 'black', display: 'block' }} // Update the color to black
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+                    <PermIdentityOutlinedIcon sx={{ color: "black", position: "relative", pr: 1.5 }} />
+                    <CasesOutlinedIcon sx={{ color: "black", position: "relative", pr: 1.5 }} />
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Box >
+
+                            <Tooltip title="Open settings">
+                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                    <MenuOutlinedIcon sx={{ color: "black", pr: 3 }} />
+
+                                </IconButton>
+                            </Tooltip>
+                        </Box>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+
+                </Toolbar>
+            </Container>
+
+        </AppBar>
+    );
+}
+export default ResponsiveAppBar;
