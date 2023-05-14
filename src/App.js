2028af6bb0d391b81f9245783490e4f5b05cfd26
ahmad amira <ahmad_amira@outlook.com>
2023-05-15 01:16:02 +0300

    Added Hero section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
+import Home from './Components/Home-page/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <Footer />
     </div>
   );
---
+++ src/Components/Home-page/Herosec/Hero.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Box, Typography, Button, useTheme } from '@mui/material';
+
+const Hero = () => {
+    const theme = useTheme();
+    return (
+        <Box display="inline-block" position="relative">
+            <img src="/imgs/img_1.png" alt="Hero Image" style={{ width: '100%', height: 'auto' }} />
+            <Box
+                position="absolute"
+                top="30%"
+                right="10%"
+                transform="translate(-50%, -50%)"
+                textAlign="center"
+
+            >
+                <Typography variant="h3" color="white" sx={{
+                    mb: 5,
+                    [theme.breakpoints.down('sm')]: {
+                        fontSize: '1.5rem',
+                    },
+                }}> STYLIST PICKS BEAT <br></br> THE HEAT</Typography>
+                <Button
+                    variant="contained"
+
+                    sx={{
+                        backgroundColor: 'transparent',
+                        border: '3px solid white',
+                        color: 'white',
+                    }}
+                >
+                    SHOP NOW
+                </Button>
+            </Box>
+        </Box>
+    );
+}
+
+export default Hero;
