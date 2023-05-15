269bb8177c0387203372f6790c4b4b2602416aa0
Ahmad Marei <82359651+101ahmadmarei@users.noreply.github.com>
2023-05-15 19:49:01 +0300

    hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './Components/Header/Header';
-import Footer from './Components/Footer/Footer';
-import Home from './Components/Home-page/Home';
-import Cardcom from './Components/Card-com/Card';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home-page/Home";
+import Cardcom from "./Components/Card-com/Card";
 
 function App() {
   return (
--- src/Components/Home-page/Herosec/Hero.js
+++ src/Components/Home-page/Herosec/Hero.js
@@ -1,40 +1,59 @@
-import React from 'react';
-import { Box, Typography, Button, useTheme } from '@mui/material';
+import React from "react";
+import { Box, Typography, Button, useTheme, Container } from "@mui/material";
+import classes from "./Hero.module.css";
 
 const Hero = () => {
-    const theme = useTheme();
-    return (
-        <Box display="inline-block" position="relative">
-            <img src="/imgs/img_1.png" alt="Hero Image" style={{ width: '100%', height: 'auto' }} />
-            <Box
-                position="absolute"
-                top="30%"
-                right="10%"
-                transform="translate(-50%, -50%)"
-                textAlign="center"
-
-            >
-                <Typography variant="h3" color="white" sx={{
-                    mb: 5,
-                    [theme.breakpoints.down('sm')]: {
-                        fontSize: '1.5rem',
-                    },
-                }}> STYLIST PICKS BEAT <br></br> THE HEAT</Typography>
-                <Button
-                    variant="contained"
-
-                    sx={{
-                        backgroundColor: 'transparent',
-                        border: '3px solid white',
-                        color: 'white',
-
-                    }}
-                >
-                    SHOP NOW
-                </Button>
-            </Box>
+  const theme = useTheme();
+  return (
+    <Box
+      display="inline-block"
+      position="relative"
+      sx={{ width: "100%" }}
+      className={classes.hero}
+    >
+      {/* <img
+        src="/imgs/img_1.png"
+        alt="Hero Image"
+        style={{ width: "100%", height: "auto" }}
+      /> */}
+      <Container maxWidth="lg" sx={{ position: "relative", height: "100%" }}>
+        <Box
+          sx={{
+            position: "absolute",
+            right: { xs: "50%", md: "30%", lg: "10%" },
+            top: "50%",
+            transform: {
+              xs: "translate(50%, -50%)",
+              lg: "translate(0%, -50%)",
+            },
+            textAlign: "center",
+            width: { xs: "100%", md: "initial" },
+          }}
+        >
+          <Typography
+            variant="h3"
+            color="white"
+            sx={{
+              mb: 5,
+              fontSize: { xs: "2rem", md: "3rem" },
+            }}
+          >
+            STYLIST PICKS BEAT <br></br> THE HEAT
+          </Typography>
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: "transparent",
+              border: "3px solid white",
+              color: "white",
+            }}
+          >
+            SHOP NOW
+          </Button>
         </Box>
-    );
-}
+      </Container>
+    </Box>
+  );
+};
 
 export default Hero;
